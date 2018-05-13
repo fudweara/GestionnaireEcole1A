7d985f3c23ee5f0d6b079a41a6c7bb7bddbc157f
Victor <victor.duclos@wanadoo.fr>
2018-05-13 19:02:19 +0200

    1.0

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import Interface.FenetreIdentification;
  * Gestionnaire ESIGELEC pour la gestion de personnes, de cartes Léo et de lieux
  *
  * @author ESERTINE Aurelien ET DUCLOS Victor
- * @version 0.3
+ * @version 1.0
  */
 class Main {
 
